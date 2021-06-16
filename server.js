4b38e791506c1b72aaea331e7fe9782203cb6e77
Divyansh Tyagi <divyansh1107@gmail.com>
2021-06-16 10:11:47 +0530

    Improved video calling functionality/ fixed crashes when user leaves call

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express')
 const app = express()
-// const cors = require('cors')
-// app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-  debug: true
-});
 const { v4: uuidV4 } = require('uuid')
 
-app.use('/peerjs', peerServer);
 
+
+// Setting up rendering method of rooms
 app.set('view engine', 'ejs')
+// Setting the static folder
 app.use(express.static('public'))
 
+
 app.get('/', (req, res) => {
+  // Create a brand new room and redirect the user there
   res.redirect(`/${uuidV4()}`)
 })
 
@@ -23,28 +21,28 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
+// Everytime someone connects to the server
 io.on('connection', socket => {
+  
+  // When someone joins the room
   socket.on('join-room', (roomId, userId) => {
-    console.log(roomId)
+    console.log("User joined " + roomId)
+    console.log("With user id " + userId)
+    // Joining the room with the current user
     socket.join(roomId)
-    socket.to(roomId).emit('user-connected', userId);
-    // messages
-    socket.on('message', (message) => {
-      //send message to the same room
-      io.to(roomId).emit('createMessage', message)
-  }); 
+    socket.broadcast.to(roomId).emit('user-connected', userId);
 
-    socket.on('end-call-server', () =>{
-      // socket.emit('leave-room-client');
-      console.log("sending wait");
-      socket.to(roomId).emit('user-disconnected', userId);
-
-    })
 
     socket.on('disconnect', () => {
+      console.log("Disconnected");
       socket.to(roomId).emit('user-disconnected', userId)
     })
-  })
+
+
+  }); 
+
+
+    
 })
 
 server.listen(process.env.PORT||3030)
