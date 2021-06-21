21e8014d1200235be0d08b61357c75b53d49d068
Divyansh Tyagi <divyansh1107@gmail.com>
2021-06-21 08:32:39 +0530

    Added authentication

---
+++ public/signup.js
@@ -0,0 +1,48 @@
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const signup_pt2 = () => {
+
+    // perform email check
+    let email = document.querySelector('#signup_email').value;
+
+    if(validateEmail(email) == false) {
+        document.querySelector('.alert').innerHTML = "Invalid Email";
+        return;
+    }
+
+    // perform password check
+    let password1 =  document.querySelector('#signup_password').value;
+    let password2 =  document.querySelector('#signup_confirm_password').value;
+    
+    if(password1== "" || password2 == "") {
+        document.querySelector('.alert').innerHTML = "Empty password";
+        return ;
+    }
+    console.log(password1, password2);
+    if(password1 != password2) {
+        // SHOW ERROR
+        document.querySelector('.alert').innerHTML = "Passwords do not match";
+        return;
+    }
+
+
+    document.querySelector('.alert').innerHTML = "";
+
+    document.querySelector('.signup_email').style.display = 'none';
+    document.querySelector('.signup_names').style.display = 'block';
+
+};
+
+const signup_pt1 = () => {
+    document.querySelector('.signup_names').style.display = 'none';
+    document.querySelector('.signup_email').style.display = 'block';
+    
+};
+
+
+const submitForm = () => {
+    document.querySelector('form').submit;
+}
--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 var random_name = require('random-name') // used for generating random names
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+// for development
+require('dotenv').config()
 
 
 // Setting up rendering method of rooms
@@ -11,46 +19,147 @@ app.set('view engine', 'ejs')
 // Setting the static folder
 app.use(express.static('public'))
 
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
 
-app.get('/', (req, res) => {
-  // Create a brand new room and redirect the user there
-  res.redirect(`/video-chat?roomId=${uuidV4()}`);
+
+
+const users = []
+
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave:false,
+  saveUninitialized : false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index', { name: req.user.name })
 })
 
-app.get('/video-chat', (req, res) => {
-  console.log(req.query);
-  res.render('room', { roomId: req.query.roomId ,userName : random_name.first()});
+
+
+app.get('/login', checkNotAuthenticated,(req, res) => {
+  res.render('login')
 })
 
-// Everytime someone connects to the server
-io.on('connection', socket => {
-  
-  // When someone joins the room
-  socket.on('join-room', (roomId, userId) => {
-    console.log("User joined " + roomId)
-    console.log("With user id " + userId)
-    // Joining the room with the current user
-    socket.join(roomId)
-    socket.broadcast.to(roomId).emit('user-connected', userId);
-
-    // Messagin functionality
-    socket.on('message-sent', (res) => {
-      console.log(res.userName);
-      //send message to the same room
-      io.to(roomId).emit('append-message', {'userName' : res.userName, 'message' : res.messageContent});
-    }); 
+app.post('/login', checkNotAuthenticated,passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
 
-    socket.on('disconnect', () => {
-      console.log("Disconnected");
-      socket.to(roomId).emit('user-disconnected', userId)
+
+app.get('/signup', checkNotAuthenticated, (req, res) => {
+  res.render('signup')
+})
+
+app.post('/signup', checkNotAuthenticated, async (req, res) => {
+  console.log(req.body);
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password1, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
     })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/signup')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
 
 
-  }); 
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
 
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+
+
+app.listen(3030)
+
+// app.get('/meeting-control',(req, res) => {
+//   res.render('meeting-control');
+// })
+
+// // app.get('/', (req, res) => {
+// //   // Create a brand new room and redirect the user there
+// //   res.redirect(`/video-chat?roomId=${uuidV4()}`);
+// // })
+
+
+
+
+// app.get('/video-chat', (req, res) => {
+//   console.log(req.query);
+//   res.render('room', { roomId: req.query.roomId ,userName : random_name.first()});
+// })
+
+// // Everytime someone connects to the server
+// io.on('connection', socket => {
+//   console.log("connected");
+//   // When someone joins the room
+//   socket.on('join-room', (roomId, userId) => {
+//     console.log("User joined " + roomId)
+//     console.log("With user id " + userId)
+//     // Joining the room with the current user
+//     socket.join(roomId)
+//     socket.broadcast.to(roomId).emit('user-connected', userId);
+
+//     // Messagin functionality
+//     socket.on('message-sent', (res) => {
+//       console.log(res.userName);
+//       //send message to the same room
+//       io.to(roomId).emit('append-message', {'userName' : res.userName, 'message' : res.messageContent});
+//     }); 
+
+
+//     socket.on('disconnect', () => {
+//       console.log("Disconnected");
+//       socket.to(roomId).emit('user-disconnected', userId)
+//     })
 
     
-})
+    
 
-server.listen(process.env.PORT||3030)
+//   }); 
+
+
+
+//   // Create new meeting url
+//   socket.on('create-meeting-url',()=>{
+//     console.log("here");
+//     socket.emit('new-meeting-url-created',uuidV4());
+//   })
+
+    
+// })
+
+// server.listen(process.env.PORT||3030)
